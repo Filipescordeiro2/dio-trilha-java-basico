a1e0f4ba01daffbb1db50db36b235af05e605c5b
filipescordeiro2 <filipescordeiro@hotmail.com>
2024-05-10 20:34:20 -0300

    Entrega do Projeto Finalizada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    // Variavel Scanner    
+    Scanner input = new Scanner(System.in);
+    
+    //Variaveis do ususario
+    String Agencia;
+    String NomeDoCliente = "MARIO ANDRADE";
+    int Numero = 1021;
+    double Saldo = 237.48;
+
+   // Interacao com ususario
+   System.out.println("Por favor, digite o número da Agência !");
+   Agencia = input.nextLine();
+   //Condicionais
+    if (Agencia.equals("067-8")) {
+        System.out.println("Olá "+NomeDoCliente+" ,obrigado por criar uma conta em nosso banco, sua agência é "+Agencia+", conta "+Numero+ " e seu saldo "+Saldo+" já está disponível para saque");
+   }else
+            System.out.println("Não Localizado");
     }
 }
